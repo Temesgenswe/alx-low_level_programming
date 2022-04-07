de9fc21ccb7174cef8cd98d030fe9040f6ef7f00
Temesgen <temesgenswe@gmail.com>
2022-04-07 13:01:47 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-*
-*Return: 0 after printing the function
-*/
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts ("\" programming is like bulding a multilingual puzzle");
- return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 }
