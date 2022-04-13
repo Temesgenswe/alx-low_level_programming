b2a5c1ef0b1d7b3c6dcc4ae4d614e7f2f4be5d10
Temesgen <temesgenswe@gmail.com>
2022-04-12 22:49:16 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+#include "main.h"                                                       
+/**                                                                     
+*main - entry block                                                     
+*@void no agrument                                                      
+*Return 0;                                                              
+**/                                                                     
+int main (void)                                                         
+{                                                                       
+char temesgen[]= "_putchar";                                            
+                                                                        
+int c;                                                                  
+for (c=0;c<8;c++)                                                       
+{                                                                       
+  _putchar(temesgen[c]);                                                
+}      
+_putchar('\n');
+return (0);
 }
