8930fa0ebd98597ca2e1d8e63daf973fb426b9ad
Temesgen <temesgenswe@gmail.com>
2022-04-14 15:57:32 -0700

    -

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,36 @@
-#include "main.h"
+#include "holberton.h"
+void print_integer(int m);
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - a function that prints an integer.
+ * @n: An input integer
+ * Return: Nothing
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
-
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
 		_putchar('-');
-		_putchar (n + '0');
+		print_integer(n * -1);
 	}
-
 	else
-	{
-		if (n < 0)
+		print_integer(n);
+}
+
+/**
+ * print_integer - A function to priting n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
 		{
-			n = n * -1;
-			_putchar ('-');
+			_putchar((m / i) % 10 + '0');
 		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
 }
