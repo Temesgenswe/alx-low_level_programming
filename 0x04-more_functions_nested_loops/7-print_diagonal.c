064b86c563fd6441ba6dc09fc7abb167cc94b101
Temesgen <temesgenswe@gmail.com>
2022-04-14 14:59:48 -0700

    task six

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal signal
+ * @n: value to create matriz
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i > j)
+<<<<<<< HEAD
+<<<<<<< HEAD
+					putchar('\\');
+			} 
+				_putchar('\n');
+=======
+=======
+>>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
+					_putchar(32);
+				if (i == j)
+					_putchar('\\');
+			}
+			_putchar('\n');
+>>>>>>> 392d491848f636c4e835156c05c1cfd685354ece
+		}
+	}
+	else
+		_putchar('\n');
+}
