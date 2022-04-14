b29af4bfefcd2d2c1a82778c2e326ef82a85d078
Temesgen <temesgenswe@gmail.com>
2022-04-14 14:51:09 -0700

    task ix

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - print character (_) n times
+ * @n: Indicates the number of times
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
