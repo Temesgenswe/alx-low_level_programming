5a286dd71158561c2c9f7579c0c247083f0ebc0c
Temesgen <temesgenswe@gmail.com>
2022-04-18 13:23:33 -0700

    third task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - a function that prints a string
+ * @str: string input
+ * Return: string
  */
-int main(void)
+void _puts(char *str)
 {
-    char *str;
+	int i;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
