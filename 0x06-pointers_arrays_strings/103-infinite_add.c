2416ca8ae24ee033188fcb4dafc662c103dc0ab2
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-20 20:55:36 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,46 +1,61 @@
 #include "main.h"
 
 /**
- * infinite_add - adds two numbers.
- * @n1:char
- * @n2:char.
- * @r:char
- * @size_r:int
- * Return:char.
- */
+  * infinite_add - add two numbers
+  * @n1: first number
+  * @n2: second number
+  * @r: the buffer
+  * @size_r: the size of r
+  * Return: char
+  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	int digit = 0;
+	unsigned int digitval;
+	unsigned int value1 = 0;
+	unsigned int value2 = 0;
+	unsigned int value3 = 0;
+	char *p = r;
+
+	while (n1[i + 1])
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		n1++;
+		i++;
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	while (i)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		value1 += n1[i] - '0';
+		i--;
 	}
-	return (r);
+	while (n2[j + 1])
+	{
+		n2++;
+		j++;
+	}
+	while (j)
+	{
+		value2 += n2[j - '0'];
+		j--;
+	}
+	value3 = value1 + value2;
+	digitval = value3;
+	while (digitval / 10)
+	{
+		count += 1;
+		digitval /= 10;
+	}
+	count += 1;
+	if (count > size_r)
+		return (0);
+	for (; count; count--)
+	{
+		digit = (value3 % 10);
+		value3 = (value3 - digit);
+		r[count] = digit + '0';
+	}
+	return (p);
 }
