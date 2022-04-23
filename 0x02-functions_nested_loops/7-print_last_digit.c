f8417b10059a341dc3db45d8a14b2693dfa4ff18
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-23 08:03:27 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,6 @@
-#include "holberton.h"
+#include "main.h"
 
-/**
- * print_last_digit - Check Holberton
- * @r: An integer input
- * Description: This function prints the last digit of a number
- * Return: last digit of number r
- */
+
 int print_last_digit(int r)
 {
 	int n;
