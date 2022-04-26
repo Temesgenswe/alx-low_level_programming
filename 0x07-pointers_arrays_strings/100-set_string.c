1e46caec03cf0de96839a66d676b2c8247a201fb
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-26 05:39:11 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -6,13 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void set_string(char **s, char *to)
 {
-    char *s0 = "Bob Dylan";
-    char *s1 = "Robert Allen";
-
-    printf("%s, %s\n", s0, s1);
-    set_string(&s1, s0);
-    printf("%s, %s\n", s0, s1);
-    return (0);
+	*s = to;
 }
