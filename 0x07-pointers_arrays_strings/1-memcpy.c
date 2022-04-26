5b2ee18f6e11f20dfb5d1b2699b22dc93f91577c
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-26 05:13:26 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *ptr = s;
+	unsigned int i;
 
-	while (n-- > 0)
-	*ptr++ = b;
-	return (s);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
