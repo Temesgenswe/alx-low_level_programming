2ffadcb6fe900570331c8e400d74d05f8a865e7e
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-27 06:48:09 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
- * @s: string
+ * main - check the code
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
