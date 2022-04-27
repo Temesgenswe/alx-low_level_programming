4849d81eb6e77dc2a47775c390285fb7eedb068d
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-04-27 06:45:29 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,9 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	_puts_recursion("Puts with recursion");
+	
 	_putchar(*s);
 	_puts_recursion(s + 1);
+	_puts_recursion("Puts with recursion");
+	return (0);
 }
