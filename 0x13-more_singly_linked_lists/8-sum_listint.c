f97f1eb38db7386114c47d87ad4decd1736f9263
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-24 20:53:08 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * sum_listint - returns the sum of all the data (n) of a listint_t list.
  * @head: pointer to the list.
- * @index: nth node.
- * Return: nth node, if the node doesn't exist, returns NULL.
+ * Return: sum of all data in the list, 0 if the lost is empty.
  **/
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int sum_listint(listint_t *head)
 {
 	listint_t *node = head;
-	unsigned int cont = 0;
+	int sum = 0;
 
-	while (node && cont != index)
+	while (node)
 	{
-		cont++;
+		sum += node->n;
 		node = node->next;
 	}
-	if (node && cont == index)
-		return (node);
-	return (NULL);
+
+	return (sum);
 }
