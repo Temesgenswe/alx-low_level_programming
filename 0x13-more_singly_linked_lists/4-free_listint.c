93cbbcc0b5c5c37627383f7c59c147469ea6f5b5
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-24 20:08:37 +0300

    Create 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *actual_node;
+	listint_t *next_node;
+
+	if (head)
+	{
+		actual_node = head;
+		next_node = head->next;
+		while (next_node)
+		{
+			free(actual_node);
+			actual_node = next_node;
+			next_node = next_node->next;
+		}
+		free(actual_node);
+	}
+}
