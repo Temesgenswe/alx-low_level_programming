b0f552b5c40701294b021da113f8c9d204999ab8
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-24 20:50:37 +0300

    Create 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+
+#include "lists.h"
+
+/**
+ * main 
+ *
+ * Return: Always 0.
+ **/ 
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node = head;
+	unsigned int cont = 0;
+
+	while (node && cont != index)
+	{
+		cont++;
+		node = node->next;
+	}
+	if (node && cont == index)
+		return (node);
+	return (NULL);
+}
