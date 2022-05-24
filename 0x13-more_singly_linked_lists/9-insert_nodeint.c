4aef00e07d31ac249d58ff724a02296c8ab18f28
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-24 21:34:34 +0300

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ *
+ **/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *aux_node = *head;
+	listint_t *new_node;
+	unsigned int index;
+	unsigned int cont = 0;
+
+	/* insert Node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	/* border case for insert at the beginning */
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (*head);
+	}
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
+	{
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->next = aux_node->next;
+		aux_node->next = new_node;
+		return (new_node);
+	}
+
+	free(new_node);
+	return (NULL);
+}
