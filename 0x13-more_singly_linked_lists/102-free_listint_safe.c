f92a7f3c99c322e9960b99104dfe71d90fdf0e97
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-25 05:51:47 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,10 +1,6 @@
 #include "lists.h"
 
-/**
- * free_listint_safe - frees a list
- * @h: the pointer to the start of list to free
- * Return: size of free'd list (in nodes?)
- */
+
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nodeCount = 0;
