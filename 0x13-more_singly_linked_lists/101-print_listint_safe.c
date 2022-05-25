befd79d643e0bc7c1df04b5294042703fde48012
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-25 05:52:12 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,6 @@
 #include "lists.h"
 
-/**
- * reverse_listint - prints a listint_t linked list.
- * @head: pointer to the list.
- * Return: number of nodes in the list.
- **/
+
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t pichu = 0;
