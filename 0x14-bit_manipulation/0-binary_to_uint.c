f14c93a92e2cd7387fb8b593aa80c645956b4392
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-26 23:58:59 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,20 +1,34 @@
-#include <stdio.h>
+
 #include "main.h"
 
+/**
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*b != '\0')
+
+	while (b[i + 1])
 	{
-		value = value << 1;
-		if (*b != '1' && *b != '0')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		else if (*b == '1')
-			value = value | 1;
-		b++;
+		i++;
 	}
-	return (value);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
