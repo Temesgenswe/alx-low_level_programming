bca09dd8605ca4d5f80dd750f2aff045c4c58efc
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-27 00:00:33 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif /*_MAIN_H_*/
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+#endif
