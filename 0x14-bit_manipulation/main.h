84f7b0d828540c14e12a58f895eaeefeadc345be
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-26 23:56:00 +0300

    Create main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /*_MAIN_H_*/
