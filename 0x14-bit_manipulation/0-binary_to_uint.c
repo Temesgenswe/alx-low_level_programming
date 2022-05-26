a74788c37ce2a06b996d0a6ff3757de6d425edd8
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-26 23:56:55 +0300

    Create 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
+}
