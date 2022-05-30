c7e820ced16c95e1362c2d92f28ff2efbed11e27
Temesgenswe <101357503+Temesgenswe@users.noreply.github.com>
2022-05-30 22:31:39 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,8 @@
-/*
-#include "holberton.h"
+
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
 char *create_buffer(char *file)
 {
 	char *buffer;
